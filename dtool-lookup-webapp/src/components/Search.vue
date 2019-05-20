18287e01b2d890df42a7a896a40f697f5942e245
Tjelvar Olsson <tjelvar.olsson@jic.ac.uk>
2019-05-20 16:05:29 +0100

    Add the actual search component file

---
+++ dtool-lookup-webapp/src/components/Search.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="search">
+    <h2>Search</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Search",
+  props: {
+    lookup_url: String
+  }
+};
+</script>
+
+<style></style>
