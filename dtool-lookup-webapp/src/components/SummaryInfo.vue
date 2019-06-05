9391839c6152832c93210d77976eb38087b12d34
Tjelvar Olsson <tjelvar.olsson@jic.ac.uk>
2019-06-05 13:27:34 +0100

    Fix lint issues and add spinner when loading

--- dtool-lookup-webapp/src/components/SummaryInfo.vue
+++ dtool-lookup-webapp/src/components/SummaryInfo.vue
@@ -1,44 +1,43 @@
 <template>
   <div>
-    <div v-if="loading">
-        <p>Loading...</p>
+    <div v-if="loading" class="spinner-border text-primary">
+      <span class="sr-only">Loading...</span>
     </div>
     <div v-else>
-        <div v-if="errored">
-            <p>Unable to load summary information please return later.</p>
-        </div>
-        <div v-else>
-            <ul class="list-group">
-                <li class="list-group-item">
-                    All
-                    <span class="badge badge-pill badge-primary">{{
-                    summary_info["number_of_datasets"]
-                    }}</span>
-                </li>
-                <li
-                    class="list-group-item"
-                    v-for="base_uri in summary_info['base_uris']"
-                    v-bind:key="base_uri"
-                >
-                    {{ base_uri }}
-                    <span class="badge badge-pill badge-secondary">{{
-                    summary_info["datasets_per_base_uri"][base_uri]
-                    }}</span>
-                </li>
-                <li
-                    class="list-group-item"
-                    v-for="creator in summary_info['creator_usernames']"
-                    v-bind:key="creator"
-                >
-                    {{ creator }}
-                    <span class="badge badge-pill badge-secondary">{{
-                    summary_info["datasets_per_creator"][creator]
-                    }}</span>
-                </li>
-            </ul>
-        </div>
+      <div v-if="errored">
+        <p>Unable to load summary information please return later.</p>
+      </div>
+      <div v-else>
+        <ul class="list-group">
+          <li class="list-group-item">
+            All
+            <span class="badge badge-pill badge-primary">{{
+              summary_info["number_of_datasets"]
+            }}</span>
+          </li>
+          <li
+            class="list-group-item"
+            v-for="base_uri in summary_info['base_uris']"
+            v-bind:key="base_uri"
+          >
+            {{ base_uri }}
+            <span class="badge badge-pill badge-secondary">{{
+              summary_info["datasets_per_base_uri"][base_uri]
+            }}</span>
+          </li>
+          <li
+            class="list-group-item"
+            v-for="creator in summary_info['creator_usernames']"
+            v-bind:key="creator"
+          >
+            {{ creator }}
+            <span class="badge badge-pill badge-secondary">{{
+              summary_info["datasets_per_creator"][creator]
+            }}</span>
+          </li>
+        </ul>
+      </div>
     </div>
-
   </div>
 </template>
 
