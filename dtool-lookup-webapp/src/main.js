0baea2a11990e4981930d17f92ba5bf1418fc65f
Tjelvar Olsson <tjelvar.olsson@jic.ac.uk>
2019-06-10 13:33:33 +0100

    Make use of bootstrap-vue

--- dtool-lookup-webapp/src/main.js
+++ dtool-lookup-webapp/src/main.js
@@ -2,9 +2,14 @@ import Vue from "vue";
 import App from "./App.vue";
 
 import axios from "axios";
+import BootstrapVue from "bootstrap-vue";
 import store from "./store";
 import VueClipboard from "vue-clipboard2";
 
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+
+Vue.use(BootstrapVue);
 Vue.use(VueClipboard);
 Vue.prototype.$http = axios;
 Vue.config.productionTip = false;
