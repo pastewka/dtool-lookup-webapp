d57f765b43d070bdffbdb4ec6a5011b31b32d73f
Tjelvar Olsson <tjelvar.olsson@jic.ac.uk>
2019-06-10 16:30:53 +0100

    Improve usefulness of dataset item list in DatasetInfo component

--- dtool-lookup-webapp/src/components/DatasetInfo.vue
+++ dtool-lookup-webapp/src/components/DatasetInfo.vue
@@ -41,10 +41,41 @@
       <ul class="list-group">
         <li
           class="list-group-item"
-          v-for="item in dataset.manifest.items"
-          v-bind:key="item"
+          v-for="(item, id) in dataset.manifest.items"
+          v-bind:key="id"
+          @mouseover="update_fetch_identifier(id)"
         >
-          {{ item.relpath }}
+          <div class="d-flex  justify-content-between">
+            <h6 class="mb-1">{{ item.relpath }}</h6>
+            <small>{{ filesize(item.size_in_bytes) }} </small>
+          </div>
+
+          <div class="d-flex  justify-content-between">
+            <small>{{ id }}</small>
+            <b-dropdown right text="Fetch" size="sm">
+              <b-dropdown-text
+                >The command below fetches the dataset item and returns an
+                absolute path on disk from where it can be
+                accessed</b-dropdown-text
+              >
+              <b-dropdown-form style="width: 300px;">
+                <b-input-group>
+                  <b-form-input
+                    v-model="fetch_command"
+                    size="sm"
+                  ></b-form-input>
+                  <b-input-group-append>
+                    <b-button
+                      size="sm"
+                      variant="outline-secondary"
+                      v-clipboard:copy="fetch_command"
+                      >cp</b-button
+                    >
+                  </b-input-group-append>
+                </b-input-group>
+              </b-dropdown-form>
+            </b-dropdown>
+          </div>
         </li>
       </ul>
     </div>
@@ -52,6 +83,7 @@
 </template>
 
 <script>
+var filesize = require("filesize");
 var moment = require("moment");
 var yaml = require("json2yaml");
 export default {
@@ -61,8 +93,10 @@ export default {
   },
   data: function() {
     return {
+      filesize: filesize,
       moment: moment,
-      yaml: yaml
+      yaml: yaml,
+      fetch_identifier: null
     };
   },
   computed: {
@@ -71,6 +105,16 @@ export default {
     },
     copy_command: function() {
       return "dtool cp " + this.dataset.uri + " .";
+    },
+    fetch_command: function() {
+      return (
+        "dtool item fetch " + this.dataset.uri + " " + this.fetch_identifier
+      );
+    }
+  },
+  methods: {
+    update_fetch_identifier(identifier) {
+      this.fetch_identifier = identifier;
     }
   }
 };
