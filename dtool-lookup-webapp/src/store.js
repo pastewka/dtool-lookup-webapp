dcec6481070ab774e156911425314968b31fc34e
Tjelvar Olsson <tjelvar.olsson@jic.ac.uk>
2020-05-10 18:25:15 +0100

    Removed offending console.log statement that prevented npm run build from completing

--- dtool-lookup-webapp/src/store.js
+++ dtool-lookup-webapp/src/store.js
@@ -21,35 +21,27 @@ export default new Vuex.Store({
       state.free_text = free_text;
     },
     update_creator_usernames(state, creator_usernames) {
-      console.log("Store: update creator usernames to: " + creator_usernames);
       state.creator_usernames = creator_usernames;
     },
     update_base_uris(state, base_uris) {
-      console.log("Store: update base URIs to: " + base_uris);
       state.base_uris = base_uris;
     },
     update_tags(state, tags) {
-      console.log("Store: update tags to: " + tags);
       state.tags = tags;
     },
     update_current_dataset_index(state, index) {
-      console.log("Store: update current dataset index");
       state.current_dataset_index = index;
     },
     update_current_dataset(state, dataset) {
-      console.log("Store: update current dataset");
       state.current_dataset = dataset;
     },
     update_current_dataset_manifest(state, manifest) {
-      console.log("Store: update current dataset manifest");
       state.current_dataset_manifest = manifest;
     },
     update_current_dataset_readme(state, readme) {
-      console.log("Store: update current dataset readme");
       state.current_dataset_readme = readme;
     },
     update_current_dataset_annotations(state, annotations) {
-      console.log("Store: update current dataset annotations");
       state.current_dataset_annotations = annotations;
     },
     update_num_filtered(state, num_filtered) {
