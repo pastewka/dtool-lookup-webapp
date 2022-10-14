962e746eb64cf4d8e156079025321b3f915b4846
Johannes Laurin Hoermann <johannes.hoermann@imtek.uni-freiburg.de>
2022-10-14 18:43:37 +0200

    MAINT: Show readme as plain text

--- dtool-lookup-webapp/src/components/DatasetReadme.vue
+++ dtool-lookup-webapp/src/components/DatasetReadme.vue
@@ -1,25 +1,8 @@
 <template>
   <div>
     <h3>Readme</h3>
-    <pre>{{ yaml.stringify(readme) }}</pre>
+    <pre>{{ readme }}</pre>
   </div>
 </template>
-
-<script>
-var yaml = require("json2yaml");
-export default {
-  name: "DatasetReadme",
-  data: function() {
-    return {
-      yaml: yaml
-    };
-  },
-  computed: {
-    readme: function() {
-      return this.$store.state.current_dataset_readme;
-    }
-  }
-};
-</script>
 
 <style></style>
