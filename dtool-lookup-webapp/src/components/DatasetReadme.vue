2f0a86db23eb0210b30b33f2606ddf5b27e29307
Johannes Laurin Hoermann <johannes.hoermann@imtek.uni-freiburg.de>
2022-10-24 10:09:28 +0200

    MAINT: Undo readme as plain text changes for now

--- dtool-lookup-webapp/src/components/DatasetReadme.vue
+++ dtool-lookup-webapp/src/components/DatasetReadme.vue
@@ -1,13 +1,19 @@
 <template>
   <div>
     <h3>Readme</h3>
-    <pre>{{ readme }}</pre>
+    <pre>{{ yaml.stringify(readme) }}</pre>
   </div>
 </template>
 
 <script>
+var yaml = require("json2yaml");
 export default {
   name: "DatasetReadme",
+  data: function() {
+    return {
+      yaml: yaml
+    };
+  },
   computed: {
     readme: function() {
       return this.$store.state.current_dataset_readme;
