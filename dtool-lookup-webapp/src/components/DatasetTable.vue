a72f0bfc4261d6015c80e038ddb2082ff32ebdda
ashdriod <ashwinvazhappilly@gmail.com>
2023-01-24 14:00:11 +0100

    WIP: pagination

--- dtool-lookup-webapp/src/components/DatasetTable.vue
+++ dtool-lookup-webapp/src/components/DatasetTable.vue
@@ -1,21 +1,24 @@
 <template>
   <div>
+
     <div class="list-group">
-      <a
-        href=""
-        class="list-group-item list-group-item-action"
-        v-for="(dataset, index) in datasetHits"
-        v-bind:class="{ active: selected === index }"
+      <a href="" class="list-group-item list-group-item-action"
+        v-for="(dataset,index) in datasetHits.slice((pageNumber - 1) * perPage, pageNumber * perPage)"
+        
+        v-bind:class="{ active: selected === index }" 
         v-bind:key="dataset.uri"
-        @click.prevent="updateSelectedDataset(index)"
-      >
+        @click.prevent="updateSelectedDataset(index)">
+        
+       
         <div class="d-flex flex-row justify-content-between">
           <h6 class="p-0">{{ dataset.name }}</h6>
           <small class="p-0">{{
-            moment(dataset.created_at*1000).format("YYYY-MM-DD")
+            moment(dataset.created_at * 1000).format("YYYY-MM-DD")
           }}</small>
         </div>
 
+      
+   
         <div class="d-flex flex-row justify-content-between">
           <small class="p-0">{{ dataset.creator_username }}</small>
           <small class="p-0">{{ dataset.uuid }}</small>
@@ -26,13 +29,32 @@
             <template v-for="(tag, index) in dataset.tags" v-bind:key="index">
               <span class="badge badge-pill badge-info bg-primary">{{
                 tag
-              }}</span
-              >{{ "&nbsp;" }}
+              }}</span>{{ "&nbsp;" }}
+        
             </template>
           </div>
         </div>
+
+
+
       </a>
+      <b-pagination 
+    v-model="pageNumber" 
+    :total-rows="rows" 
+    :per-page="perPage" 
+    first-text="First" 
+    prev-text="Prev" 
+    next-text="Next" 
+    last-text="Last">
+  </b-pagination>
+
+
     </div>
+
+
+
+
+
   </div>
 </template>
 
@@ -43,18 +65,23 @@ export default {
   props: {
     datasetHits: Array
   },
-  data: function() {
+  data: function () {
     return {
-      moment: moment
+      moment: moment,
+      rows: Math.ceil(this.datasetHits.length*10),
+      perPage: 3,
+      pageNumber: 1,
+      sb:1,
     };
   },
   computed: {
-    selected: function() {
+   
+    selected: function () {
       return this.$store.state.current_dataset_index;
     }
   },
   methods: {
-    updateSelectedDataset: function(index) {
+    updateSelectedDataset: function (index) {
       this.$store.commit("update_current_dataset_index", index);
       this.$store.commit("update_current_dataset", this.datasetHits[index]);
       this.$emit("update-dataset");
@@ -63,4 +90,6 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+
+</style>
