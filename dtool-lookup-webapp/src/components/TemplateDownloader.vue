b2dd69b561993d54be77d8fb6e7f17de240c5800
ashdriod <ashwinvazhappilly@gmail.com>
2024-04-28 18:11:58 +0200

    WIP:Download

--- dtool-lookup-webapp/src/components/TemplateDownloader.vue
+++ dtool-lookup-webapp/src/components/TemplateDownloader.vue
@@ -1,22 +1,38 @@
 <template>
-    <div>
-        <button
-                class="btn btn-outline-danger"
-                type="button"
-                @click="logout"
-              >
-                Logout
-              </button>
-    </div>
-  </template>
-  
+  <div>
+    <BDropdown
+      id="account-dropdown"
+      v-model="show1"
+      text="Account"
+      variant="primary"
+      class="ml-auto"
+    >
+      <BDropdownItem @click="downloadFile('yaml')"
+        >Download Readme as YAML</BDropdownItem
+      >
+
+      <BDropdownItem @click="downloadFile('json')"
+        >Download Readme as JSON</BDropdownItem
+      >
+
+      <BDropdownItem @click="logout">
+        <button class="btn btn-outline-danger" type="button">Logout</button>
+      </BDropdownItem>
+    </BDropdown>
+
+    <button class="btn btn-outline-danger" type="button" @click="logout">
+      Logout
+    </button>
+  </div>
+</template>
+
 <script>
 export default {
   name: "TemplateDownloader",
   methods: {
     logout() {
-        this.$emit("logoutAction");
-    }
-  }
+      this.$emit("logoutAction");
+    },
+  },
 };
 </script>
