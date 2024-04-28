56ac69fbe87c1051441e053a5b69808f733ee1ae
ashdriod <ashwinvazhappilly@gmail.com>
2024-04-28 20:07:52 +0200

    WIP:Download

--- dtool-lookup-webapp/src/components/TemplateDownloader.vue
+++ dtool-lookup-webapp/src/components/TemplateDownloader.vue
@@ -1,38 +1,44 @@
 <template>
-  <div>
-    <BDropdown
-      id="account-dropdown"
-      v-model="show1"
-      text="Account"
-      variant="primary"
-      class="ml-auto"
-    >
-      <BDropdownItem @click="downloadFile('yaml')"
-        >Download Readme as YAML</BDropdownItem
-      >
-
-      <BDropdownItem @click="downloadFile('json')"
-        >Download Readme as JSON</BDropdownItem
-      >
-
+    <!-- Render the logout button if all conditions are false -->
+    <button class="btn btn-outline-danger" type="button" @click="logout" v-if="!downloadReadmeYaml && !downloadReadmeJson && !showInfoMenuEntry">
+      Logout
+    </button>
+  
+    <!-- Render the dropdown menu if any of the conditions are true -->
+    <BDropdown id="account-dropdown" v-model="show1" text="Account" variant="primary" class="ml-auto" v-else>
+      <BDropdownItem @click="downloadFile('yaml')">
+        Download dtool_readme.yml
+      </BDropdownItem>
+  
+      <BDropdownItem @click="downloadFile('json')">
+        Download dtool.json
+      </BDropdownItem>
+  
       <BDropdownItem @click="logout">
         <button class="btn btn-outline-danger" type="button">Logout</button>
       </BDropdownItem>
     </BDropdown>
-
-    <button class="btn btn-outline-danger" type="button" @click="logout">
-      Logout
-    </button>
-  </div>
-</template>
+  </template>
 
 <script>
+import { BDropdown, BDropdownItem } from "bootstrap-vue-next";
 export default {
-  name: "TemplateDownloader",
-  methods: {
-    logout() {
-      this.$emit("logoutAction");
+    name: "TemplateDownloader",
+    data() {
+        return {
+            downloadReadmeYaml: process.env.VUE_APP_OFFER_DTOOL_README_YAML_DOWNLOAD|| false,
+            downloadReadmeJson: process.env.VUE_APP_OFFER_DTOOL_JSON_DOWNLOAD || false,
+            showInfoMenuEntry : process.env.VUE_APP_SHOW_INFO_MENU_ENTRY || false,
+        };
     },
-  },
+    components: {
+        BDropdown,
+        BDropdownItem
+    },
+    methods: {
+        logout() {
+            this.$emit("logoutAction");
+        }
+    }
 };
 </script>
